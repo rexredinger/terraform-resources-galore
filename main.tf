3a5482639a11ecd11a7b29ed3b2836ea7a07a777
Alexander Redinger <redingera@gmail.com>
2021-10-28 13:51:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,49 @@
 # Workspace variables:
+# Create them in this order exactly!
 
 # Terraform variables:
-# DOUBLED_VAR = 'terraform-value'
 # TERRAFORM_ONLY_VAR = 'has-a-value'
+# DOUBLED_VAR_TF_CREATED_FIRST = 'tf-value'
 
 # Environment variables:
-# DOUBLED_VAR = 'environment-value'
 # ENV_ONLY_VAR = 'has-a-value'
+# DOUBLED_VAR_TF_CREATED_FIRST = 'env-value'
+# DOUBLED_VAR_ENV_CREATED_FIRST = 'env-value'
 
-variable "DOUBLED_VAR" {}
+# Terraform variable:
+# DOUBLED_VAR_ENV_CREATED_FIRST = 'tf-value'
+
+variable "DOUBLED_VAR_TF_CREATED_FIRST" {
+  default = "default-value"
+}
+
+variable "DOUBLED_VAR_ENV_CREATED_FIRST" {
+  default = "default-value"
+}
 
 variable "TERRAFORM_ONLY_VAR" {}
 
-
-resource "null_resource" "check_doubled_var" {
+resource "null_resource" "check_doubled_var_created_first_as_tf_var" {
   provisioner "local-exec" {
-    # expect this to output DOUBLED_VAR=terraform-value, and NOT
-	# reflect the environment variable value
-    command = "env | grep DOUBLED_VAR || echo 'doubled var not found'"
+    # expect this to output DOUBLED_VAR_TF_CREATED_FIRST=tf-value, and NOT
+    # reflect the environment variable value
+    command = "env | grep DOUBLED_VAR_TF_CREATED_FIRST || echo 'doubled var created as tf var first not found'"
+  }
+  triggers = { random = uuid() }
+}
+
+resource "null_resource" "check_doubled_var_created_first_as_env_var" {
+  provisioner "local-exec" {
+    # expect this to output DOUBLED_VAR_ENV_CREATED_FIRST=env-value, and NOT
+    # reflect the terraform variable value
+    command = "env | grep DOUBLED_VAR_ENV_CREATED_FIRST || echo 'doubled var created as env var first not found'"
   }
   triggers = { random = uuid() }
 }
 
 resource "null_resource" "check_terraform_var" {
   provisioner "local-exec" {
-  	# expect this to output TERRAFORM_ONLY_VAR=has-a-value
+    # expect this to output TERRAFORM_ONLY_VAR=has-a-value
     command = "env | grep TERRAFORM_ONLY_VAR || echo 'tf var not found as env var'"
   }
   triggers = { random = uuid() }
@@ -38,9 +57,14 @@ resource "null_resource" "check_env_only_var" {
   triggers = { random = uuid() }
 }
 
-output "doubled_variable" {
-  # expect this to output 'terraform-value'
-  value = var.DOUBLED_VAR
+output "doubled_variable_tf_created_first" {
+  # expect this to output 'tf-value'
+  value = var.DOUBLED_VAR_TF_CREATED_FIRST
+}
+
+output "doubled_variable_env_created_first" {
+  # expect this to output 'default-value'
+  value = var.DOUBLED_VAR_ENV_CREATED_FIRST
 }
 
 output "terraform_only_var" {
