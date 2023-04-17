7d0108d39083ba7607191692c77248c58680c4c1
Alexander Redinger <redingera@gmail.com>
2023-04-17 12:13:16 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,26 +6,3 @@ resource "tfe_workspace" "initiald" {
   organization = var.organization
   name = "Initial_D"
 }
-
-check "Initial_D_check" {
-  data "tfe_workspace" "ws" {
-    name = "Initial_D"
-    organization = var.organization
-  }
-  
-  assert {
-    condition = data.tfe_workspace.ws.name == "Initial_D"
-    error_message = "Your Initial_D workspace isn't anymore"
-  }
-}
-
-locals {
-   timetime = timestamp()
-}
-
-check "time_thing" {
-  assert {
-    condition = timecmp(local.timetime, timestamp()) > 0
-    error_message = "wut?"
-  }
-}
