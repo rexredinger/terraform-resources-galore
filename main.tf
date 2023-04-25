8a97a4bc929cd50ba722889c60e6ee7510c35510
Rexredinger <redinger@hashicorp.com>
2023-04-25 11:54:22 -0400

    fixed tf version

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ resource "tfe_workspace" "pets" {
   count = var.workspace_count
   organization = var.organization
   name = format("pets_workspace_%s", count.index)
-  execution_mode = "1.4.5"
+  execution_mode = "agent"
   auto_apply = true
-  terraform_version = latest
+  terraform_version = "latest"
 
   vcs_repo {
     identifier = "rexredinger/terraform-resources-galore"
