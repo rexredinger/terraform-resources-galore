e6dbdc728a102719e8a4cb539a867af23b095773
Rexredinger <redinger@hashicorp.com>
2023-04-24 20:18:27 -0400

    random pets

--- main.tf
+++ main.tf
@@ -1,22 +1,19 @@
-provider "tfe" {
-  hostname = var.hostname
-}
-
-data "tfe_workspace" "parent" {
-  name = var.parent_workspace
-  organization = var.organization
-}
-
-resource "tfe_workspace" "initiald" {
-  count = var.workspace_count
-  organization = var.organization
-  name = format("workspace_%s", count.index)
-  execution_mode = "agent"
-
-  vcs_repo {
-    identifier = tfe_workspace.parent.vcs_repo.identifier
-    branch = (count.index%2 == 0) ? "pets" : "numbers"
-    oauth_token_id = tfe_workspace.parent.vcs_repo.oauth_token_id
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
   }
 }
 
+provider "random" {
+}
+
+resource "random_pet" "pet" {
+  count = 4
+}
+
+output "critter" {
+  value = random_pet.pet.*
+}
