3a3cf8ce34d10a31373a154865592470237f8be1
Rexredinger <redinger@hashicorp.com>
2023-04-24 20:41:11 -0400

    just pets, diff tf versions

--- main.tf
+++ main.tf
@@ -7,15 +7,21 @@ data "tfe_workspace" "parent" {
   organization = var.organization
 }
 
-resource "tfe_workspace" "initiald" {
+resource "random_shuffle" "tf_version" {
+  input = ["1.2.4", "1.3.1", "1.4.5", "latest"]
+}
+
+resource "tfe_workspace" "pets" {
   count = var.workspace_count
   organization = var.organization
-  name = format("workspace_%s", count.index)
+  name = format("pets_workspace_%s", count.index)
   execution_mode = "agent"
+  auto_apply = true
+  terraform_version = random_shuffle.tf_version.result[0]
 
   vcs_repo {
     identifier = tfe_workspace.parent.vcs_repo.identifier
-    branch = (count.index%2 == 0) ? "pets" : "numbers"
+    branch = "pets"
     oauth_token_id = tfe_workspace.parent.vcs_repo.oauth_token_id
   }
 }
